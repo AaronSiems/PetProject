be8d3c0d705ed5c345b10bbc8b962d1e15b54074
Aaron <unconfigured@null.spigotmc.org>
2019-02-27 00:31:58 -0600

    Create LocalDate converter
    Created the localdate converter

---
+++ src/main/java/controller/LocalDateConverter.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+
+@Converter(autoApply = true)
+public class LocalDateConverter implements AttributeConverter<LocalDate, Date> {
+	
+	public LocalDateConverter() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	//@Override
+	public Date convertToDatabaseColumn(LocalDate attribute) {
+		// TODO Auto-generated method stub
+		return (attribute == null ? null: Date.valueOf(attribute));
+	}
+	//@Override
+	public LocalDate convertToEntityAttribute(Date dbData) {
+		// TODO Auto-generated method stub
+		return(dbData == null ? null: dbData.toLocalDate());
+	}
+	
+	
+}
+
